daa963926cf899dab95efd71a8f5b7d027e7bc06
arpanDeb19 <arpandebtest@gmail.com>
2024-03-18 00:44:51 +0530

    normal change

---
+++ script.js
@@ -0,0 +1,9 @@
+function beforesubmit(){
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value :", inputdate.value); //string --> date (en_IN)
+
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  console.log("Formatted Date :", formattedDate);
+  outputdate.value = formattedDate;
+}
