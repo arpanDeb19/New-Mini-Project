ef1c1a1fe5a45fff5c241aa7503d45b1284e12ab
arpanDeb19 <arpandebtest@gmail.com>
2024-03-24 13:12:32 +0530

    Normal Updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let captchachecked = false;
-function beforesubmit(event){
-  if(captchachecked){
+function beforesubmit(event) {
+  if (captchachecked) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value :", inputdate.value); //string --> date (en_IN)
@@ -12,20 +12,22 @@ function beforesubmit(event){
     alert("Please check the reCaptcha box to submit the lead");
     event.preventDefault();
   }
-  
 }
 
-function timestamp() { 
-var response = document.getElementById("g-recaptcha-response"); 
-    if (response == null || response.value.trim() == ""){
-      var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-      elems["ts"] = JSON.stringify(new Date().getTime());
-      document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);       
-    } 
-  } 
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
 
 setInterval(timestamp, 500);
 
-function capthchasuccess(){
-  captchachecked =true;
+function capthchasuccess() {
+  captchachecked = true;
 }
